de317891db2f3f3f3a1bd5c263225abbdaad17f8
IAmPicard <tekmanro@yahoo.com>
2017-10-03 16:01:07 -0700

    Initial commit of the STTApi, copied wholesale from the spreadsheet project with minor changes to export things as a module.

---
+++ lib/computeWorker.ts
@@ -0,0 +1,58 @@
+export class MinimalComplement {
+	unneededCrew: Array<number>;
+    neededCrew: Array<number>;
+}
+
+export function computeCrewSuccessmissionSuccess(missionSuccess: any): MinimalComplement {
+    let baseline = 0;
+    let allConsideredCrew = new Set<number>();
+    missionSuccess.forEach((entry: any) => {
+        entry.crew.forEach((crew: any) => {
+            allConsideredCrew.add(crew.crew.id);
+        });
+
+        baseline += (entry.crew.length > 0) ? entry.crew[0].success : 0;
+    });
+
+    // TODO: This should actually do a combinatorial (all possible combinations of crew ids from allConsideredCrew).
+    // However, that pegs the CPU for minutes even on a fast i7-7700k
+    // The algorithm below is suboptimal but it's much cheaper to run
+
+    // Calculate minimal set of crew out of allConsideredCrew that still yields the same result for all challenges
+    let start = allConsideredCrew.size;
+
+    let removedCrew = new Set<number>();
+
+    let before;
+    do {
+        before = allConsideredCrew.size;
+
+        for (let crewId of allConsideredCrew) {
+            let result = 0;
+
+            missionSuccess.forEach((entry: any) => {
+                let filteredCrew = entry.crew.filter((crew: any) => { return (crew.crew.id != crewId) && !removedCrew.has(crew.crew.id); });
+                result += (filteredCrew.length > 0) ? filteredCrew[0].success : 0;
+            });
+
+            if (result == baseline) {
+                allConsideredCrew.delete(crewId);
+                removedCrew.add(crewId);
+                break;
+            }
+        }
+    } while (allConsideredCrew.size < before);
+
+    return {
+        unneededCrew: Array.from(removedCrew),
+        neededCrew: Array.from(allConsideredCrew)
+    };
+}
+
+//TODO: Figure out if we can use workers in typescript modules
+/*
+// silly patch to make typescript happy
+const sendMessage: any = self.postMessage;
+self.addEventListener('message', (message) => {
+    sendMessage(computeCrewSuccessmissionSuccess(message.data.success));
+});*/
---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+export * from "./STTApi";
+
+export { mergeDeep } from './ObjectMerge';
+export { getWikiImageUrl } from './WikiImageTools';
+export { loginSequence } from './LoginSequence';
+export { loadFullTree } from './EquipmentTools';
+export { loadGauntlet, gauntletCrewSelection, gauntletRoundOdds, payToGetNewOpponents, playContest } from './GauntletTools';
