38a44941adf60fa30b054e82852c69ab68f771e4
IAmPicard <tekmanro@yahoo.com>
2017-10-15 10:33:29 -0700

    More icons

--- lib/CONFIG.ts
+++ lib/CONFIG.ts
@@ -69,6 +69,7 @@ export default class CONFIG {
 		'pp_currency_icon': { asset: 'atlas_stt_icons', url: undefined }, // dilithium
 		'soft_currency_icon': { asset: 'atlas_stt_icons', url: undefined }, // credits
 		'victory_point_icon': { asset: 'atlas_stt_icons', url: undefined },
+		'energy_icon': { asset: 'atlas_stt_icons', url: undefined },
 		'images_currency_honor_currency_0': { asset: '', url: undefined },
 		'images_currency_pe_currency_0': { asset: '', url: undefined },
 		'images_currency_pp_currency_0': { asset: '', url: undefined },
